78da76b52094d7624fea632675e8c8cfd00d5e5b
leftidev <jes.bergstrom@gmail.com>
2024-10-04 11:25:15 +0300

    Implemented parts 2.6-2.10.

---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,17 @@
+const Person = ({ person }) => {
+    return (
+      <div>{person.name} {person.number}</div>
+    )
+  }
+  
+  const Persons = ({ persons }) => {
+    return (
+      <div>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </div>
+    )
+  }
+  
+  export default Persons;
