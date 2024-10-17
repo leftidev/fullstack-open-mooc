76f895fec305f0bbd51466c829ad5add79c98f10
leftidev <jes.bergstrom@gmail.com>
2024-10-17 11:49:10 +0300

    Implemented part 4.2.

---
+++ part4/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+pp.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
