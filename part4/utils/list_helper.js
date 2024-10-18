672a7beaf8b2535d0573eaedf1e32d8f9cf10c27
leftidev <jes.bergstrom@gmail.com>
2024-10-18 10:13:33 +0300

    Implemented part 4.5.

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,20 @@ const totalLikes = (blogs) => {
     return blogs.reduce((n, {likes}) => n + likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null
+
+    const mostLikedBlog = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+
+    return {
+        title: mostLikedBlog.title,
+        author: mostLikedBlog.author,
+        likes: mostLikedBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
