1df5b07dda4a44f0f01deb07e0d13269239c2db1
leftidev <jes.bergstrom@gmail.com>
2024-10-21 10:21:08 +0300

    Implemented parts 4.10 and 4.11.

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -42,6 +42,28 @@ test.only('a specific blog can be viewed', async () => {
   assert.deepStrictEqual(resultBlog.body, blogToView)
 })
 
+test.only('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: "Async A. Wait Caller",
+    url: "http://example.com"
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(n => n.title)
+  assert(titles.includes('async/await simplifies making async calls'))
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
